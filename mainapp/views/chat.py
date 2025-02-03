bd6c5f3b9d46725a89312a150a2b9de76134ff19
tiken14 <tiken14@live.com>
2025-02-03 15:19:16 +0500

    работа над чатом

--- mainapp/views/chat.py
+++ mainapp/views/chat.py
@@ -14,11 +14,20 @@ class ChatListView(ListView):
         context = super().get_context_data(object_list=None, **kwargs)
         user = self.request.user
         chats = Chat.objects.filter(Q(request__owner=user) | Q(created_by=user))
-
         context['chats'] = chats
-
         return context
 
 
 class ChatDetailView(DetailView):
-    pass
+    model = Chat
+    template_name = 'mainapp/chats/chat-detail.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=None, **kwargs)
+        user = self.request.user
+        chats = Chat.objects.filter(Q(request__owner=user) | Q(created_by=user))
+        messages = Message.objects.all()
+        context['chats'] = chats
+        context['messages'] = messages
+
+        return context
