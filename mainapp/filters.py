28c2bbb3eb539670ee704f22c6d92406a90426ca
tiken14 <tiken14@live.com>
2025-02-11 17:07:26 +0500

    Правка стилей

--- mainapp/filters.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-@register.filter
-def readed_messages(value, arg):
-    # return [x for x in value if x.readed == arg]
-    return value * arg
